58720c2b6d891c12dcc8fd3896629e3146e705ac
Felipe Cárdenas <felipecardenas301@gmail.com>
2023-05-12 18:18:09 -0400

    [update] Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import eslint from 'vite-plugin-eslint'
+import { Connectors } from './models/db/connectors'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -7,16 +8,22 @@ export default defineNuxtConfig({
 		initRootEmail: '',
 		databaseUri: '',
 		authSecret: '',
+		connector: 'sqlite' as keyof typeof Connectors,
 	},
 	typescript: {
 		strict: true,
 		typeCheck: true,
+		tsConfig: {
+			exclude: ['node_modules'],
+		},
 	},
 	css: ['@/assets/scss/main.scss', '@/assets/scss/_z-index.scss'],
 	modules: [
 		'@nuxt/content',
 		'@pinia/nuxt',
 		'@sidebase/nuxt-auth',
+		'@nuxtjs/i18n',
+		'nuxt-icon',
 		// 'nuxt-security',
 	],
 	auth: {
@@ -24,6 +31,24 @@ export default defineNuxtConfig({
 	},
 	imports: {
 		dirs: ['stores'],
+	},
+	i18n: {
+		locales: [
+			{
+				code: 'en',
+				name: 'English',
+				file: 'en-US.json',
+			},
+			{
+				code: 'es',
+				name: 'Español',
+				file: 'es.json',
+			},
+		],
+		lazy: true,
+		defaultLocale: 'es',
+		langDir: 'lang',
+		strategy: 'prefix_except_default',
 	},
 	vite: {
 		css: {
